3e3513bd603f741ecd074e2e87a67e52c2bf2239
mesmith454 <mesmith454@gmail.com>
2020-11-17 15:18:01 -0800

    commit

--- newsletter.rb
+++ newsletter.rb
@@ -3,7 +3,6 @@ require 'pry'
 #########################
 # Data for the newsletter
 #########################
-require "pry"
 CAMPUS = {
   "name": "DC",
   "address": "1440 G St NW, Washington, DC 20005",
@@ -28,21 +27,30 @@ ARTICLES = [
 #########################
 
 def calculate_recipients
+  subscribed = []
+  SUBSCRIBERS.find_all do |email|
+    if UNSUBSCRIBED.find email == SUBSCRIBERS[index]
+      SUBSCRIBERS[index].delete
+    else
+      subscribed << SUBSCRIBERS[index]
+    end
+  end
+    puts subscribed
   # Using the SUBSCRIBERS and UNSUBSCRIBED arrays,
   # write a method that will return an array of only the subscribers who haven't unsubscribed
 end
 
-def first_n_articles(number_of_articles
-  ARTICLES.first(number_of_articles)
+def first_n_articles(number_of_articles)
+  ARTICLES.size(number_of_articles)
 end
-
-def print_recipients
-  # Write a method that uses the output of calculate_recipients
-  # and returns a list of emails separated by commas
-  # Ex) "abc@email.com, def@email.com, ghi@email.com"
+def print_recipients(calculate_recipients)
+  subscribed.split
+  puts subscribed + ", "
 end
 
 def print_one_article(article)
+  ARTICLES.find do |title, author, text|
+    puts " #{title} #{author} #{text}"
   # Write a method that will take an article hash
   # and print the title, author and text as a formatted string
   # See the README/sample output for examples
@@ -54,7 +62,7 @@ def print_many_articles(articles)
 end
 
 def format_campus_location(campus)
-  "Flatiron #{campus["name"]}"
+  "Flatiron #{CAMPUS["name"]}"
 end
 
 def format_subject
@@ -79,8 +87,6 @@ def print_newsletter(number)
   articles = first_n_articles(number)
   print_many_articles(articles)
   puts format_footer(CAMPUS)
-
-  end
 end
 
 def run
@@ -92,3 +98,5 @@ end
 # When we run "ruby newsletter.rb" in the command line,
 # the 'run' method will be called because we're calling it below.
 run
+
+ 
